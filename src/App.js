87495ce4db7d9be7d96b4eef3f4a660b0e24552d
Sean Kramer <seandominickramer@gmail.com>
2020-04-15 16:29:47 -0400

    Set use state for apod

--- src/App.js
+++ src/App.js
@@ -3,11 +3,16 @@ import "./App.css";
 import axios from 'axios'
 
 function App() {
+  const [apod, setApod] = useState(null)
+
+
+  
   return (
     <div className="App">
+      <h1>NASA Image of the Day</h1>
+      <img src={apod} alt="Astronomy Picture of the Day"/>
       <p>
-        Read through the instructions in the README.md file to build your NASA
-        app! Have fun <span role="img" aria-label='go!'>🚀</span>!
+      One of the most popular websites at NASA is the Astronomy Picture of the Day. In fact, this website is one of the most popular websites across all federal agencies. It has the popular appeal of a Justin Bieber video. This endpoint structures the APOD imagery and associated metadata so that it can be repurposed for other applications.
       </p>
     </div>
   );
