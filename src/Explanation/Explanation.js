839234f742c02583290979c89364ea76cc4b5e5a
Sean Kramer <seandominickramer@gmail.com>
2020-04-16 18:52:44 -0400

    Add toast component from reactstrap

--- src/Explanation/Explanation.js
+++ src/Explanation/Explanation.js
@@ -1,9 +1,20 @@
 import React, { useState, useEffect } from "react";
-import "./Explanation.css"
+// import "./Explanation.css";
+import { Toast, ToastBody, ToastHeader } from 'reactstrap';
 
 const Explanation = ({ apod }) => {
   return (
-    <p className='explanationText'>{apod && apod.explanation}</p>
+    // <p className='explanationText'>{apod && apod.explanation}</p>
+    <div className="p-3 my-2 rounded" style={{ background: 'black' }}>
+    <Toast>
+      <ToastHeader>
+      {apod && apod.title}
+      </ToastHeader>
+      <ToastBody>
+      {apod && apod.explanation}
+      </ToastBody>
+    </Toast>
+  </div>
   )
   
 };
